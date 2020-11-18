ee6faeceaa3bb78f91cc0589f981b3cdb6442b40
Jayson Rice <jayson.rice.95@gmail.com>
2020-11-18 17:14:17 -0500

    Initial ball physics.

--- sketch.js
+++ sketch.js
@@ -1,26 +1,62 @@
-const canvasW = 512;
-const canvasH = 512;
 
-let y = 0;
-let speed = 4;
+let ball;
 
 function setup() {
-  createCanvas(canvasW, canvasH);
+  createCanvas(windowWidth, windowHeight);
+  ball = new Ball(width / 2, 0, 50);
 }
 
 function draw() {
-  background(25);
-  stroke(255);
-  strokeWeight(4);
-  noFill();
-  ellipse(canvasH / 2, y, 100, 100);
+  // background(25);
+  ball.update()
+  ball.draw()
+}
 
-  if (y > height) {
-    speed = -4;
+function mouseClicked() {
+  ball.clickEvent(mouseX, mouseY)
+}
+
+class Ball {
+  constructor(x, y, radius) {
+    this.pos = createVector(x, y)
+    this.vel = createVector()
+    this.acc = createVector()
+    this.hitVelocity = createVector()
+    this.hitMagnitude = 30
+    this.radius = radius
+    this.gravity = createVector(0, 0.5)
+    this.speedLimit = 20
   }
 
-  y += speed;
+  clickEvent(x, y) {
+
+    if (dist(this.pos.x, this.pos.y, x, y) < this.radius) {
+      const angle = atan2(y - this.pos.y, x - this.pos.x)
+      this.hitVelocity.y = 0;
+      this.hitVelocity.x = -this.hitMagnitude;
+      this.hitVelocity.rotate(angle)
+      print("hit")
+    }
+    else print("whoops")
+  }
+
+  update() {
+    this.vel.add(this.acc)
+    this.vel.add(this.hitVelocity)
+    this.vel.add(this.gravity)
+    this.vel.limit(this.speedLimit)
+    this.pos.add(this.vel)
+  }
+
+  draw() {
+    ellipse(this.pos.x, this.pos.y, this.radius * 2, this.radius * 2)
+  }
+
+
+
+
 }
+
 
 window.setup = setup;
 window.draw = draw;
