d19a6706f5344d5d2fd143a48da64fa753fe3c70
AdamSpannbauer <spannbaueradam@gmail.com>
2020-11-18 17:27:10 -0500

    eslint

--- sketch.js
+++ sketch.js
@@ -1,5 +1,41 @@
-
 let ball;
+
+class Ball {
+  constructor(x, y, radius) {
+    this.pos = createVector(x, y);
+    this.vel = createVector();
+    this.acc = createVector();
+    this.hitVelocity = createVector();
+    this.hitMagnitude = 30;
+    this.radius = radius;
+    this.gravity = createVector(0, 0.5);
+    this.speedLimit = 20;
+  }
+
+  clickEvent(x, y) {
+    if (dist(this.pos.x, this.pos.y, x, y) < this.radius) {
+      const angle = atan2(y - this.pos.y, x - this.pos.x);
+      this.hitVelocity.y = 0;
+      this.hitVelocity.x = -this.hitMagnitude;
+      this.hitVelocity.rotate(angle);
+      print('hit');
+    } else {
+      print('whoops');
+    }
+  }
+
+  update() {
+    this.vel.add(this.acc);
+    this.vel.add(this.hitVelocity);
+    this.vel.add(this.gravity);
+    this.vel.limit(this.speedLimit);
+    this.pos.add(this.vel);
+  }
+
+  draw() {
+    ellipse(this.pos.x, this.pos.y, this.radius * 2, this.radius * 2);
+  }
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -8,55 +44,14 @@ function setup() {
 
 function draw() {
   // background(25);
-  ball.update()
-  ball.draw()
+  ball.update();
+  ball.draw();
 }
 
 function mouseClicked() {
-  ball.clickEvent(mouseX, mouseY)
+  ball.clickEvent(mouseX, mouseY);
 }
-
-class Ball {
-  constructor(x, y, radius) {
-    this.pos = createVector(x, y)
-    this.vel = createVector()
-    this.acc = createVector()
-    this.hitVelocity = createVector()
-    this.hitMagnitude = 30
-    this.radius = radius
-    this.gravity = createVector(0, 0.5)
-    this.speedLimit = 20
-  }
-
-  clickEvent(x, y) {
-
-    if (dist(this.pos.x, this.pos.y, x, y) < this.radius) {
-      const angle = atan2(y - this.pos.y, x - this.pos.x)
-      this.hitVelocity.y = 0;
-      this.hitVelocity.x = -this.hitMagnitude;
-      this.hitVelocity.rotate(angle)
-      print("hit")
-    }
-    else print("whoops")
-  }
-
-  update() {
-    this.vel.add(this.acc)
-    this.vel.add(this.hitVelocity)
-    this.vel.add(this.gravity)
-    this.vel.limit(this.speedLimit)
-    this.pos.add(this.vel)
-  }
-
-  draw() {
-    ellipse(this.pos.x, this.pos.y, this.radius * 2, this.radius * 2)
-  }
-
-
-
-
-}
-
 
 window.setup = setup;
 window.draw = draw;
+window.mouseClicked = mouseClicked;
