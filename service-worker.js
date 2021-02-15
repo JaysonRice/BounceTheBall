b710379f9665fe8d8bd6dda47f7595d6ba0a50f6
Adam Spannbauer <spannbaueradam@gmail.com>
2021-02-15 05:15:39 -0500

    rm console.log

--- service-worker.js
+++ service-worker.js
@@ -44,10 +44,7 @@ const urlsToCache = [
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      }),
+      .then((cache) => cache.addAll(urlsToCache)),
   );
 });
 
